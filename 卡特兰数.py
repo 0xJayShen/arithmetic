13ebc6f8b503eaa8e324375397467e4da90455ab
Jay <976739120@qq.com>
2019-03-26 16:02:45 +0800

    新增

--- 卡特兰数.py
+++ 卡特兰数.py
@@ -1,7 +1,17 @@
 # -*- coding: utf8 -*-
-def catalan(n):
-    if n==0 or n==1:
-        return 1
-    return ((4*n-2)*catalan(n-1))/(n+1)
-res = catalan(6)
-print(res)
+import ccxt
+import time
+exchange_class = getattr(ccxt, 'huobipro')
+exchange = exchange_class({
+    'apiKey': "8a145ec9-3b4261d8-06eb8713-f3ad3",
+    'secret': "5ee7bf92-57d5a5f2-1afb2dd4-30efe",
+    'timeout': 30000,
+    'enableRateLimit': False,
+})
+exchange.options['createMarketBuyOrderRequiresPrice'] = False
+while True:
+    try:
+        res = exchange.create_market_buy_order('TOP/HT', 200)
+    except Exception as e:
+        time.sleep(0.1)
+
